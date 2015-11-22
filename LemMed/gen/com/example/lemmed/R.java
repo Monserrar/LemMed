9103cefb3b5b602dc02eff086d50dd5fa72caf18
Monserrar <wcfcintra@yahoo.com.br>
2015-11-21 22:52:38 -0300

    LemMed (Sab)
    Desenvolvimento de toda parte gráfica, criação do DB, tabelas Paciente e
    Remédios, Metodos para Inserção nessas Tabelas, Navegação e
    instanciamento dos controles a serem visiveis nas activits

--- LemMed/gen/com/example/lemmed/R.java
+++ LemMed/gen/com/example/lemmed/R.java
@@ -23,89 +23,175 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int logo_lemed=0x7f020001;
-        public static final int logo_lemed_peb=0x7f020002;
+        public static final int info=0x7f020001;
+        public static final int logo_lemed=0x7f020002;
+        public static final int logo_lemed_peb=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080016;
-        public static final int btAjuda=0x7f080005;
-        public static final int btCancela_Sair=0x7f08000a;
-        public static final int btMedHora=0x7f080004;
-        public static final int btMedicamentos=0x7f080003;
-        public static final int btOk_Sair=0x7f080009;
-        public static final int btPaciente=0x7f080002;
-        public static final int btSair=0x7f080007;
-        public static final int btSair_Medicacao=0x7f08000d;
-        public static final int btSobre=0x7f080006;
-        public static final int imgViewLogoPB=0x7f080001;
-        public static final int textView1=0x7f08000c;
-        public static final int textView2=0x7f08000e;
-        public static final int textView3=0x7f08000f;
-        public static final int textView4=0x7f080010;
-        public static final int textView5=0x7f080011;
-        public static final int textView6=0x7f080012;
-        public static final int textView7=0x7f080013;
-        public static final int textView8=0x7f080014;
-        public static final int textView9=0x7f080015;
+        public static final int Ajuda_btSair=0x7f080004;
+        public static final int Main_btAjuda=0x7f080009;
+        public static final int Main_btMedicacao=0x7f080008;
+        public static final int Main_btPaciente=0x7f080006;
+        public static final int Main_btRemedios=0x7f080007;
+        public static final int Main_btSair=0x7f08000b;
+        public static final int Main_btSobre=0x7f08000a;
+        public static final int Medicacao_btExcluir=0x7f080012;
+        public static final int Medicacao_btLoc=0x7f080010;
+        public static final int Medicacao_btNovo=0x7f08000f;
+        public static final int Medicacao_btOk=0x7f080014;
+        public static final int Medicacao_btSair=0x7f080015;
+        public static final int Medicacao_btSalvar=0x7f080011;
+        public static final int Medicacao_edDataFim=0x7f080016;
+        public static final int Medicacao_edDataIni=0x7f08000e;
+        public static final int Medicacao_edHoraIni=0x7f08001c;
+        public static final int Medicacao_edHoraInterval=0x7f08001e;
+        public static final int Medicacao_edIdMedicacao=0x7f080018;
+        public static final int Medicacao_spNomeMedicamento=0x7f08001b;
+        public static final int Medicacao_spNomePaciente=0x7f08001a;
+        public static final int Paciente_btExcluir=0x7f080024;
+        public static final int Paciente_btLoc=0x7f080022;
+        public static final int Paciente_btNovo=0x7f080021;
+        public static final int Paciente_btOk=0x7f080026;
+        public static final int Paciente_btSair=0x7f080027;
+        public static final int Paciente_btSalvar=0x7f080023;
+        public static final int Paciente_edIdPaciente=0x7f080029;
+        public static final int Paciente_edIdade=0x7f080028;
+        public static final int Paciente_edLocalizar=0x7f080025;
+        public static final int Paciente_edNomePaciente=0x7f080020;
+        public static final int Remedio_btExcluir=0x7f08002e;
+        public static final int Remedio_btLoc=0x7f08002c;
+        public static final int Remedio_btNovo=0x7f08002b;
+        public static final int Remedio_btOk=0x7f080030;
+        public static final int Remedio_btSair=0x7f080033;
+        public static final int Remedio_btSalvar=0x7f08002d;
+        public static final int Remedio_edApresentacao=0x7f080031;
+        public static final int Remedio_edForma=0x7f08002a;
+        public static final int Remedio_edId=0x7f080032;
+        public static final int Remedio_edNomeRemedio=0x7f080034;
+        public static final int Remedio_edTipo=0x7f080035;
+        public static final int Remedio_edtLocalizar=0x7f08002f;
+        public static final int Sair_btCancela=0x7f080037;
+        public static final int Sair_btOk=0x7f080036;
+        public static final int Sobre_btSair=0x7f080040;
+        public static final int action_settings=0x7f080041;
+        public static final int edtLocalizar_Medicacao=0x7f080013;
+        public static final int imgViewLogoPB=0x7f080005;
+        public static final int textView1=0x7f080002;
+        public static final int textView2=0x7f08003d;
+        public static final int textView3=0x7f08003e;
+        public static final int textView4=0x7f08003f;
+        public static final int textView5=0x7f08003c;
+        public static final int textView6=0x7f080038;
+        public static final int textView7=0x7f080039;
+        public static final int textView8=0x7f08003a;
+        public static final int textView9=0x7f08003b;
         public static final int widget0=0x7f080000;
-        public static final int widget40=0x7f080008;
-        public static final int widget75=0x7f08000b;
+        public static final int widget40=0x7f080001;
+        public static final int widget41=0x7f08000c;
+        public static final int widget44=0x7f08000d;
+        public static final int widget47=0x7f080017;
+        public static final int widget49=0x7f080019;
+        public static final int widget53=0x7f08001d;
+        public static final int widget55=0x7f08001f;
+        public static final int widget75=0x7f080003;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_sair=0x7f030001;
-        public static final int activity_sobre=0x7f030002;
+        public static final int activity_ajuda=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_medicacao=0x7f030002;
+        public static final int activity_paciente=0x7f030003;
+        public static final int activity_remedio=0x7f030004;
+        public static final int activity_sair=0x7f030005;
+        public static final int activity_sobre=0x7f030006;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int sair=0x7f070001;
-        public static final int sobre=0x7f070002;
+        public static final int ajuda=0x7f070000;
+        public static final int main=0x7f070001;
+        public static final int medicacao=0x7f070002;
+        public static final int paciente=0x7f070003;
+        public static final int remedio=0x7f070004;
+        public static final int sair=0x7f070005;
+        public static final int sobre=0x7f070006;
     }
     public static final class string {
+        /**  Página Ajuda 
+         */
+        public static final int Ajuda_btSair=0x7f05000a;
+        public static final int Ajuda_lbSubTitulo=0x7f05000b;
+        public static final int Ajuda_mtxDescricao=0x7f05000c;
+        /**  Página Boas Vindas 
+         */
+        public static final int Boasvindas_btOk=0x7f05000d;
+        public static final int Main_btAjuda=0x7f050011;
+        public static final int Main_btMedicacao=0x7f050010;
+        /**  Página Main 
+         */
+        public static final int Main_btPaciente=0x7f05000e;
+        public static final int Main_btRemedio=0x7f05000f;
+        public static final int Main_btSair=0x7f050013;
+        public static final int Main_btSobre=0x7f050012;
+        public static final int Medicacao_btExcluir=0x7f050018;
+        public static final int Medicacao_btLocalizar=0x7f050015;
+        public static final int Medicacao_btNovo=0x7f050016;
+        public static final int Medicacao_btOk=0x7f050019;
+        public static final int Medicacao_btSair=0x7f05001a;
+        public static final int Medicacao_btSalvar=0x7f050017;
+        /**  Página Medicação 
+         */
+        public static final int Medicacao_edPesquisar=0x7f050014;
+        public static final int Paciente_btExcluir=0x7f05002e;
+        public static final int Paciente_btLocalizar=0x7f05002b;
+        public static final int Paciente_btNovo=0x7f05002c;
+        public static final int Paciente_btOk=0x7f05002f;
+        public static final int Paciente_btSair=0x7f050030;
+        public static final int Paciente_btSalvar=0x7f05002d;
+        public static final int Paciente_edPesquisar=0x7f05002a;
+        public static final int Paciente_lbId=0x7f050029;
+        public static final int Paciente_lbIdade=0x7f050028;
+        /**  Página Paciente 
+         */
+        public static final int Paciente_lbNomePaciente=0x7f050027;
+        public static final int Remedio_btExcluir=0x7f050024;
+        public static final int Remedio_btLocalizar=0x7f050021;
+        public static final int Remedio_btNovo=0x7f050022;
+        public static final int Remedio_btOk=0x7f050025;
+        public static final int Remedio_btSair=0x7f050026;
+        public static final int Remedio_btSalvar=0x7f050023;
+        public static final int Remedio_edPesquisar=0x7f050020;
+        public static final int Remedio_lbApresentacao=0x7f05001e;
+        public static final int Remedio_lbForma=0x7f05001d;
+        public static final int Remedio_lbId=0x7f05001f;
+        /**  Página Remedio (Medicamento) 
+         */
+        public static final int Remedio_lbNomeRemedio=0x7f05001b;
+        public static final int Remedio_lbTipo=0x7f05001c;
+        public static final int Sair_btCancela=0x7f050034;
+        public static final int Sair_btOk=0x7f050033;
+        public static final int Sair_lbConfirma=0x7f050032;
+        /**  Página Sair 
+         */
+        public static final int Sair_lbTitulo=0x7f050031;
+        public static final int Sobre_btSair=0x7f05003e;
+        /**  Página Sobre 
+         */
+        public static final int Sobre_lbAPPNome=0x7f050035;
+        public static final int Sobre_lbCadeira=0x7f050037;
+        public static final int Sobre_lbFaculdade=0x7f050036;
+        public static final int Sobre_lbP1=0x7f05003a;
+        public static final int Sobre_lbP2=0x7f05003b;
+        public static final int Sobre_lbP3=0x7f05003c;
+        public static final int Sobre_lbP4=0x7f05003d;
+        public static final int Sobre_lbProfessor=0x7f050038;
+        public static final int Sobre_lbTime=0x7f050039;
+        public static final int Sobre_titleRemedio=0x7f05003f;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
-        /** Página Ajuda
-         */
-        public static final int btAjuda_Sair=0x7f05000a;
-        /** Página Boas Vindas
-         */
-        public static final int btBoasVindas_Ok=0x7f05000b;
-        public static final int btMain_Ajuda=0x7f05000f;
-        public static final int btMain_Medicacao=0x7f05000e;
-        public static final int btMain_Medicamento=0x7f05000d;
-        /** Página Main
-         */
-        public static final int btMain_Paciente=0x7f05000c;
-        public static final int btMain_Sair=0x7f050011;
-        public static final int btMain_Sobre=0x7f050010;
-        public static final int btSair_Cancela=0x7f050015;
-        public static final int btSair_Ok=0x7f050014;
-        public static final int btSobre_Sair=0x7f050020;
         public static final int hello_world=0x7f050001;
-        public static final int lbSair_Confirma=0x7f050013;
-        /** Página Medicação
-Página Medicamento
-Página Paciente
-Página Sair
-         */
-        public static final int lbSair_Titulo=0x7f050012;
-        public static final int lbSobre_APPNome=0x7f050017;
-        public static final int lbSobre_Cadeira=0x7f050019;
-        public static final int lbSobre_Faculdade=0x7f050018;
-        public static final int lbSobre_P1=0x7f05001c;
-        public static final int lbSobre_P2=0x7f05001d;
-        public static final int lbSobre_P3=0x7f05001e;
-        public static final int lbSobre_P4=0x7f05001f;
-        public static final int lbSobre_Professor=0x7f05001a;
-        public static final int lbSobre_Time=0x7f05001b;
-        /** Página Sobre
-         */
-        public static final int lbSobre_TituloAc=0x7f050016;
         public static final int title_activity_ajuda=0x7f050003;
         public static final int title_activity_boas_vindas=0x7f050006;
         public static final int title_activity_medicacao=0x7f050007;
-        public static final int title_activity_medicamento=0x7f050009;
         public static final int title_activity_paciente=0x7f050008;
+        public static final int title_activity_remedio=0x7f050009;
         public static final int title_activity_sair=0x7f050005;
         public static final int title_activity_sobre=0x7f050004;
     }
--- LemMed/src/com/example/lemmed/MainActivity.java
+++ LemMed/src/com/example/lemmed/MainActivity.java
@@ -1,16 +1,441 @@
 package com.example.lemmed;
 
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	// Instancia do BD
+	SQLiteDatabase bancoDados;
+	
+	//Variável do tipo Cursor para Utilização nas Consultas (PONTEIRO)
+	Cursor resultadoConsultaSQL;
+	
+	//Definição da Variável do Tipo SPinner para Forma Remédio
+	private static final String[] FormaRemedio ={"Selecione","Comprimido","Cápsula","Líquido","Pomada","Pasta","Colírio","Outros"};
+
+	//Definição da Variável do Tipo SPinner para Tipo Remédio
+	private static final String[] TipoRemedio ={"Selecione","Analgésicos","Anti-Térmicos","Anti_Estamínicos","Anti-Inflamatórios","Antibióticos","Antigripais","antifungicos","Outros"};
+
+	
+	//Adaptador que transforma as Strings em Array, em elementos Spinner
+	ArrayAdapter<String> aFormaRemedio;
+	ArrayAdapter<String> aTipoRemedio;
+	
+	//Cria Spinner
+	Spinner spnFormaRemedio;
+	Spinner spnTipoRemedio;
+	
+	//Variável para Spinners
+	int idSpinnerFormaRemedio;
+	int idSpinnerTipoRemedio;
+	
+	//Criando as Variáveis que irão armazenar o conteúdo da tela de cadatro
+	//na Tabela Paciente
+	EditText edNomePaciente, edIdadePaciente, edLocalizarPaciente;
+	
+	//Criando as Variáveis que irão armazenar o conteúdo da tela de cadatro
+	//na Tabela Remédio
+	EditText edNomeRemedio, edTipoRemedio,edFormaRemedio,edApresentacaoRemedio, edLocalizarRemedio;
+
+	
+	//Usar na Listagem
+	TextView txtCodigo, txtDestino, txtTotalViagem;
+	
+	
+	//Outras Variáveis
+	int numreq, pos;
+
+
+	
+	
+		
+ /********************** Para exibir mensages: Com Alerta e Totast *******************/
+	
+	//Mostra Menssagem
+	public void showMessage(String mensagem, String titulo){
+	    
+	    AlertDialog.Builder dialogo = new AlertDialog.Builder(MainActivity.this);
+	    
+	    dialogo.setTitle(titulo);
+	    dialogo.setMessage(mensagem);
+	    dialogo.setNeutralButton("OK", null);
+	    dialogo.setIcon(R.drawable.info);
+	    dialogo.show();
+	}
+		
+	
+	//Mostra Menssagem com Confirmação
+	public void showMessageConfirma(String mensagem){
+
+		Toast.makeText(getApplicationContext(), mensagem, Toast.LENGTH_LONG).show();
+	}
+	
+
+	
+ /************************ Métodos para Chamada de Telas *****************************/
+	
+	//Chama Tela Principal (Main)
+	public void CarregarMainActivit(){
+		try{
+		//Informa a Tela que irá aparecer
+			setContentView(R.layout.activity_main);
+		
+		//Informa os controles que estarão presentes e associa aos criados em XML
+			Button Main_btPaciente = (Button) findViewById(R.id.Main_btPaciente);
+			Button Main_btRemedios = (Button) findViewById(R.id.Main_btRemedios);
+			Button Main_btSobre = (Button) findViewById(R.id.Main_btSobre);
+			Button Main_btAjuda = (Button) findViewById(R.id.Main_btAjuda);
+			Button Main_btSair = (Button) findViewById(R.id.Main_btSair);
+			
+			
+		
+		}catch (Exception e){
+			  showMessage(e.toString(),"Erro Chamar a Tela Principal");
+		   }
+			
+	}	
+	
+	//Chama Tela Paciente
+	public void CarregarPacienteActivit(){
+		try{
+		setContentView(R.layout.activity_paciente); 
+		
+		
+		//Informa os controles que estarão presentes e associa aos criados em XML
+		
+		//Botoes
+		Button Paciente_btLocalizar = (Button) findViewById(R.id.Paciente_btLoc);
+		Button Paciente_btNovo = (Button) findViewById(R.id.Paciente_btNovo);
+		Button Paciente_btSalvar = (Button) findViewById(R.id.Paciente_btSalvar);
+		Button Paciente_btExcluir = (Button) findViewById(R.id.Paciente_btExcluir);
+		Button Paciente_btOk = (Button) findViewById(R.id.Paciente_btOk);
+		Button Paciente_btSair = (Button) findViewById(R.id.Paciente_btSair);
+		
+		//EditText
+		EditText Paciente_edNomePaciente = (EditText)findViewById(R.id.Paciente_edNomePaciente);
+		EditText Paciente_edIdadePaciente = (EditText)findViewById(R.id.Paciente_edIdade);
+		
+		
+		}catch (Exception e){
+			  showMessage(e.toString(),"Erro Chamar a Tela Pacientes");
+		   }
+		
+	}
+
+	//Chama Tela Sobre
+	public void CarregarSobreActivit(){
+		try{		
+		setContentView(R.layout.activity_sobre);
+		
+		//Informa os controles que estarão presentes e associa aos criados em XML
+		
+		//Botoes
+		Button Sobre_btSair = (Button) findViewById(R.id.Sobre_btSair);
+		
+		}catch (Exception e){
+			  showMessage(e.toString(),"Erro Chamar a Tela Sobre");
+		   }
+				
+	}
+	
+	//Chama Tela Ajuda
+	public void CarregarAjudaActivit(){
+		try{		
+		setContentView(R.layout.activity_ajuda);
+		
+		//Informa os controles que estarão presentes e associa aos criados em XML
+		
+		//Botoes
+		Button Ajuda_btSair = (Button) findViewById(R.id.Ajuda_btSair);
+		
+		
+		
+		}catch (Exception e){
+			  showMessage(e.toString(),"Erro Chamar a Tela Ajuda");
+		   }
+		
+	}
+	
+	//Chama Tela Sair
+		public void CarregarSairActivit(){
+			try{			
+			setContentView(R.layout.activity_sair);
+			
+			//Informa os controles que estarão presentes e associa aos criados em XML
+			
+			//Botoes
+			Button Sair_btOk = (Button) findViewById(R.id.Sair_btOk);		
+			Button Sair_btCancela = (Button) findViewById(R.id.Sair_btCancela);
+			
+			
+			}catch (Exception e){
+				  showMessage(e.toString(),"Erro Chamar a Tela Sair");
+			   }
+			
+		}
+		
+	//Chama Tela Remedio
+	public void CarregarRemedioActivit(){
+		try{			
+		setContentView(R.layout.activity_remedio);
+		
+		//Informa os controles que estarão presentes e associa aos criados em XML
+		
+		
+		//EditText
+		EditText Remedio_edNomeRemedio = (EditText)findViewById(R.id.Remedio_edNomeRemedio);
+		EditText Remedio_edTipo = (EditText)findViewById(R.id.Remedio_edTipo);
+		EditText Remedio_edForma = (EditText)findViewById(R.id.Remedio_edForma);
+		EditText Remedio_edApresentacao = (EditText)findViewById(R.id.Remedio_edApresentacao);
+		EditText Remedio_edId = (EditText)findViewById(R.id.Remedio_edId);
+		
+		
+		//Botoes
+		Button Remedio_btLocalizar = (Button) findViewById(R.id.Remedio_btLoc);
+		Button Remedio_btNovo = (Button) findViewById(R.id.Remedio_btNovo);
+		Button Remedio_btSalvar = (Button) findViewById(R.id.Remedio_btSalvar);
+		Button Remedio_btExcluir = (Button) findViewById(R.id.Remedio_btExcluir);
+		Button Remedio_btOk = (Button) findViewById(R.id.Remedio_btOk);
+		Button Remedio_btSair = (Button) findViewById(R.id.Remedio_btSair);
+		
+		}catch (Exception e){
+			  showMessage(e.toString(),"Erro Chamar a Tela Remédios");
+		   }
+		
+	}
+	
+	//Chama Tela Medicação
+	public void CarregarMedicacaoActivit(){
+		try{		
+		setContentView(R.layout.activity_medicacao);
+		
+		
+		
+		
+		//Botoes
+		Button Medicacao_btLocalizar = (Button) findViewById(R.id.Medicacao_btLoc);
+		Button Medicacao_btNovo = (Button) findViewById(R.id.Medicacao_btNovo);
+		Button Medicacao_btSalvar = (Button) findViewById(R.id.Medicacao_btSalvar);
+		Button Medicacao_btExcluir = (Button) findViewById(R.id.Medicacao_btExcluir);
+		Button Medicacao_btOk = (Button) findViewById(R.id.Medicacao_btOk);
+		Button Medicacao_btSair = (Button) findViewById(R.id.Medicacao_btSair);
+		
+		
+		
+		}catch (Exception e){
+			  showMessage(e.toString(),"Erro Chamar a Tela Medicação");
+		   }
+		
+	}
+	
+
+/************************ Métodos para Criar o Banco e as Entidades e Atributos *****************************/
+	
+	
+	//Cria o Banco de Dados após ser Estanciado no Inicio do Código
+	public void CriarBanco(){
+		try{
+			
+			//Criar o Banco Já Estanciado no Inicio do Código
+			bancoDados = openOrCreateDatabase("BDLemMed", Context.MODE_PRIVATE, null);
+			
+		   }catch (Exception e){
+			  showMessage(e.toString(),"Erro ao Criar o Banco de Dados LemMed");
+		   }
+		
+		
+		try{
+			//Criar a Entidade de Pacientes
+		    bancoDados.execSQL("CREATE TABLE IF NOT EXISTS Tbl_Pacientes"+
+							"( IdPaciente INTEGER PRIMARY KEY AUTOINCREMENT, "+
+							" NomePaciente char(40 NOT NULL,"+
+							" Idade int(1) NOT NULL, "+
+							" tipo_viagem char(10 NOT NULL,"+
+							" data_viagem VARCHAR(10),"+
+							" numero_pessoa int, "+
+							" total_viagem REAL, "+
+							" viagem_realizada char(1) );");
+
+		   }catch (Exception e){
+			  showMessage(e.toString(),"Erro ao Criar a Tabela Pacientes");
+		   }
+		
+		try{
+			//Criar a Entidade de Remedios (Medicamentos)
+		    bancoDados.execSQL("CREATE TABLE IF NOT EXISTS Tbl_Remedios"+
+							"( IdRemedio INTEGER PRIMARY KEY AUTOINCREMENT, "+
+							" NomeRemedio char(30 NOT NULL,"+
+							" TipoRemedio char(20 NOT NULL,"+
+							" FormaRemedio char(20),"+
+							" ApresentacaoRemedio char(20) );");
+
+		   }catch (Exception e){
+			  showMessage(e.toString(),"Erro ao Criar a Tabela Remedios");
+		   }
+		
+		
+	}
+	
+	
+/************************ Métodos para Manipular as Tabelas(Entidades) e Atributos *****************************/
+
+//Entidade Pacientes
+	public void limpaCampos_Paciente(){
+	
+
+	
+	}
+	
+	public void cadastrarPaciente(View v){
+		
+		try{
+			edNomePaciente		= (EditText) findViewById(R.id.Paciente_edNomePaciente);
+			edIdadePaciente 	= (EditText) findViewById(R.id.Paciente_edIdade);
+
+			
+
+			//executa a SQL de insert na Entidade tbl_Pacientes			
+			bancoDados.execSQL("INSERT INTO tbl_Pacientes (NomePaciente,IdadePaciente)"+
+					   " VALUES('"+edNomePaciente.getText().toString()				 +
+					   "','"+ edIdadePaciente.getText().toString() 					 +
+					   "')");
+				
+			//Exibe a mensagem que Cadastrou com sucesso
+			showMessageConfirma("LemMed - Cadastro realizado com sucesso.");
+				
+			//Limpa TODOS os campos
+			limpaCampos_Paciente();
+				
+		}catch	(Exception e){
+			showMessage("Erro ao cadastrar","Erro");
+			showMessage(e.toString(),"Erro");
+			 
+		}//fecha o catch	
+	}//fecha o metodo cadastrar	
+
+	public void editarPaciente(View v){
+	
+
+}
+
+	public void excluirPaciente(View v){
+	
+	}
+
+	public void localizarPaciente(View v){
+		
+		
+	}
+	
+//Entidade Remedios
+	public void limparCampos_Remedios(){
+		
+		
+	}
+
+	public void cadastrarRemedio(View v){
+		
+		try{
+			
+			edNomeRemedio =(EditText) findViewById(R.id.Remedio_edNomeRemedio);
+			edTipoRemedio =(EditText) findViewById(R.id.Remedio_edTipo);
+			edFormaRemedio =(EditText) findViewById(R.id.Remedio_edForma);
+			edApresentacaoRemedio =(EditText) findViewById(R.id.Remedio_edApresentacao);
+			edLocalizarRemedio =(EditText) findViewById(R.id.Paciente_edLocalizar);
+
+
+			//executa a SQL de insert na Entidade tbl_Pacientes			
+			bancoDados.execSQL("INSERT INTO tbl_Remedios (NomeRemedio,TipoRemedio,FormaRemedio,ApresentacaoRemedio)"+
+					   " VALUES('"+edNomeRemedio.getText().toString()				 +
+					   "','"+ edTipoRemedio.getText().toString() 					 +
+					   "','"+ edFormaRemedio.getText().toString() 					 +
+					   "','"+ edApresentacaoRemedio.getText().toString() 			 +
+					   "')");
+				
+			//Exibe a mensagem que Cadastrou com sucesso
+			showMessageConfirma("LemMed - Cadastro realizado com sucesso.");
+				
+			//Limpa TODOS os campos
+			limpaCampos_Paciente();
+				
+		}catch	(Exception e){
+			showMessage("Erro ao cadastrar Remédio","Erro");
+			showMessage(e.toString(),"Erro");
+			 
+		}//fecha o catch	
+		
+	}
+	
+	
+/************************ Navegação entre Activits *****************************/	
+	
+
+public void botaoPaciente(View v){
+	CarregarPacienteActivit();
+}
+
+public void botaoRemedio(View v){
+	CarregarRemedioActivit();
+}
+
+public void botaoMedicacao(View v){
+	CarregarMedicacaoActivit();
+}
+
+public void botaoSobre(View v){
+	CarregarSobreActivit();
+}
+
+public void botaoAjuda(View v){
+	CarregarAjudaActivit();
+}
+	
+public void botaoSair(View v){
+	CarregarSairActivit();
+}
+
+public void botaoVoltar(View v){
+	CarregarMainActivit();
+}
+
+public void botaoSairCancela(View v){
+	CarregarMainActivit();
+}
+
+public void botaoSairOk(View v){
+
+		Intent intent = new Intent(getApplicationContext(), Activity.class);
+	    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+	    intent.putExtra("EXIT", true);
+	    startActivity(intent);
+}
+
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		CriarBanco();
 	}
 
 	@Override
